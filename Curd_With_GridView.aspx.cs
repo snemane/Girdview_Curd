7b1386a0d9afa4bacbb3506822fd390092a4140a
snemane <136227421+snemane@users.noreply.github.com>
2023-10-12 16:32:30 +0530

    Add files via upload

---
+++ Curd_With_GridView.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_With_Curd_Gridview
+{
+    public partial class Curd_With_GridView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string stron = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(stron);
+            string sql = "insert into selfinfo(name,email,phone)values('" + name.Text + "','" + email.Text + "','" + phone.Text + "');";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteReader();
+        }
+    }
+}
